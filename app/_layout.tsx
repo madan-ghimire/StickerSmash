b0f7096e22d6c526cfa2a5fd227653118defbebc
madanghimire-3ptrades <madanghimire-3ptrades@3ptrades.com>
2025-10-30 11:00:55 +0545

    configure status bar, splash screen and app icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerTitleAlign: "center" }}>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Stack screenOptions={{ headerTitleAlign: "center" }}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="light" />
+    </>
   );
 }
