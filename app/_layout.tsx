ef7fbdd32426f331e621faea432971601e485d90
madanghimire-3ptrades <madanghimire-3ptrades@3ptrades.com>
2025-10-30 20:37:35 +0545

    configure splash screen and app icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,15 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <Stack screenOptions={{ headerTitleAlign: "center" }}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" options={{}} />
       </Stack>
       <StatusBar style="light" />
-    </>
+    </GestureHandlerRootView>
   );
 }
